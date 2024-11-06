1fedc43398995912b7eab219d18cdd2dd4cf03a3
Feeh219 <168578282+Feeh219@users.noreply.github.com>
2024-11-06 10:56:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'WandaVision',
+    'Qual o nome dos filhos da Wanda e Visao?',
+    'Billy e Tommy Maximoff'
+)
+
+criaCartao(
+    'WandaVsion',
+    'Qual o nome da cidade onde se passa WandaVision?',
+    'Westview'
+)
+
+criaCartao(
+    'Agatha Desde Sempre',
+    'Por que Nicholas morreu cedo?',
+    'o filho da Agatha, morreu porque estava destinado a nascer morte, entretanto Rio deu tempo a Agtha para aproveitar Nicholas, mas a morte nao pode ser evitada'
+)
+
+criaCartao(
+    'Agatha Desde Sempre',
+    'Qual o motivo da Agatha odiar a Rio?',
+    'Pois Rio teve que levar Nicholas, mesmo tendo dado tempo a Agatha. Mesmo tendo dado tempo, a morte de Nicholas era algo inevitavel e Agatha nunca perdoo a Rio por fazer seu trabalho'
+)
